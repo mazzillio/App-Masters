f6857ffa3926c40f3d149ad452a3e411630504b0
mazzillio <mateca500@gmail.com>
2022-07-24 10:29:25 -0300

    Criação do servidor]

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+
+const serverExpress = express();
+const cors = (request, response, next) => { 
+    response.header("Access-Control-Allow-Origin", "*"); 
+    response.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+};
+serverExpress.use(express.json());
+serverExpress.use(cors);
+serverExpress.get("/", (request, response) => {
+    return response.json({ alive:false });
+});
+export { serverExpress};
