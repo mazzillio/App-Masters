e18a06c63a51a9c8d3d58e8a63df0d47262d6b40
mazzillio <mateca500@gmail.com>
2022-07-24 11:04:03 -0300

    Criação do teste

---
+++ src/test.test.js
@@ -0,0 +1,11 @@
+import request from "supertest";
+import { serverExpress } from"./server.js";
+describe("Teste da rota / do servidor", () => {
+    it("Deve ser possivel obter status 200 do servidor", async() =>{
+        const response = await request(serverExpress)
+        .get("/")
+        .set("Accept", "application/json");
+        expect(response.body).toStrictEqual({ alive:true });
+        expect(response.status).toBe(200);
+    });
+});
