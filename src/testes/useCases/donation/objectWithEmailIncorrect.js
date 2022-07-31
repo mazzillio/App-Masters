4793b5acd96b51f82a68ff8f92d63b2374ac0300
mazzillio <mateca500@gmail.com>
2022-07-31 19:51:26 -0300

    Testes do caso de uso Donation

---
+++ src/testes/useCases/donation/objectWithEmailIncorrect.js
@@ -0,0 +1,17 @@
+export const objectWithEmailIncorrect = {
+    name:"mattheus",
+    email:"mate2iosd.com",
+    phone:"(38)3213-1291",
+    city:"moc",
+    state: "minas fgerais",
+    streetAddress:"rua b",
+    zip:"39404404",
+    number:"490",
+    complement:"casa",
+    neighborhood:"santos dumont",
+    deviceCount:2,
+    devices: [
+        {type:"notebook", condition:"working"},
+        {type:"desktop", condition:"working"}
+    ]
+}
