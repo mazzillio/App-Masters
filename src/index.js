a232f60b8253805f0f8dd09cb2d34d11ef7a3521
mazzillio <mateca500@gmail.com>
2022-08-02 18:58:27 -0300

    preparando a aplicação para o deploy no heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { serverExpress } from "./server.js";
 
-const PORT = 3242;
+const PORT = process.env.PORT || 3242;
 serverExpress.listen(PORT, 
     () => console.info(`Servidor rodando na porta: ${PORT}`)
 );
