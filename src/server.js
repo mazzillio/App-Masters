c81b9f717c065d010fb90033712cf154c5e96a27
mazzillio <mateca500@gmail.com>
2022-08-04 19:14:19 -0300

    Resolução de cors

--- src/server.js
+++ src/server.js
@@ -1,16 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { AppError } from "./AppError.js";
 import { DonationUseCase } from "./useCases/DonationUseCase.js";
 
 
 const serverExpress = express();
-const cors = (request, response, next) => { 
-    response.header("Access-Control-Allow-Origin", "*"); 
-    response.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-};
 serverExpress.use(express.json());
-serverExpress.use(cors);
+serverExpress.use(cors());
 serverExpress.get("/", (request, response) => {
     return response.json({ alive: true });
 });
