e14224aafb90e1f89a165c44303644a23d259e5d
mazzillio <mateca500@gmail.com>
2022-08-04 20:15:33 -0300

    Retirando a obrigatoriedade dos campos email e complement

--- src/useCases/validations/verifyFieldsEmpty.js
+++ src/useCases/validations/verifyFieldsEmpty.js
@@ -1,9 +1,10 @@
 export function VerifyFieldsEmpty(objetoRequest) {
     const fieldsEmpty = [];
+    const optionalsFiels =["email","complement"]
     for(const keyField in objetoRequest) {
         const valueField = objetoRequest[keyField];
         if(keyField !== "deviceCount" && valueField !==0) {
-            if(!valueField) {
+            if(!optionalsFiels.includes(keyField) && !valueField) {
                 fieldsEmpty.push(keyField);
             }
         }
